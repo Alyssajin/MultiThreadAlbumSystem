cc3256348da5c6805ba9ef4bb480985238b0eaaf
alyssajin <alyssajin2019@gmail.com>
2025-02-22 21:05:11 -0800

    fix: change IPAddr, file path, parameter types in the post method, add the monitor of success and failures

--- HW4a_Client_Part1&2/src/main/java/ClientGet.java
+++ HW4a_Client_Part1&2/src/main/java/ClientGet.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
 import org.apache.hc.client5.http.async.methods.SimpleRequestBuilder;
@@ -18,12 +20,15 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 
 public class ClientGet implements Runnable {
+  private static AtomicInteger successCount = new AtomicInteger(0);
+  private static AtomicInteger failCount = new AtomicInteger(0);
+
   private String getUrl;
   private CloseableHttpClient client;
   private List<Row> data;
 
   public ClientGet(String IPAddr, CloseableHttpClient client, List<Row> data) {
-    this.getUrl = "http://" + IPAddr + "/IGORTON/AlbumStore/1.0.0/albums/1";
+    this.getUrl = "http://" + IPAddr + "/album/1";
     this.client = client;
     this.data = data;
   }
@@ -43,23 +48,37 @@ public class ClientGet implements Runnable {
      */
     try {
       long start = System.currentTimeMillis();
+      System.out.println("GET START: " + start + Thread.currentThread().getName());
       // Execute the method.
       CloseableHttpResponse response = client.execute(getMethod);
 
       int statusCode = response.getCode();
-      if (statusCode != HttpStatus.SC_OK) {
-        System.err.println("Method failed: " + statusCode);
+
+      if (statusCode >= 200 && statusCode < 300) {
+        successCount.incrementAndGet();
+      } else {
+        failCount.incrementAndGet();
+        System.err.println("Post Method failed: " + statusCode);
       }
 
       // Read the response body.
       byte[] responseBody = response.getEntity().getContent().readAllBytes();
       //System.out.println(new String(responseBody));
       long end = System.currentTimeMillis();
+      System.out.println("GET END: " + end + Thread.currentThread().getName());
       long latency = end - start;
       data.add(RowFactory.create(start, "GET", latency, statusCode));
 
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  public static int getSuccessCount() {
+    return successCount.get();
+  }
+
+  public static int getFailCount() {
+    return failCount.get();
   }
 }
--- HW4a_Client_Part1&2/src/main/java/Main.java
+++ HW4a_Client_Part1&2/src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+
 import org.apache.hc.client5.http.impl.DefaultHttpRequestRetryStrategy;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -13,63 +14,57 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.StructType;
 
-
+// Delete data.csv folder each time before running the program
 public class Main {
+  private static final int GROUP_SIZE = 10;
+  private static final int NUMBER_OF_GROUPS = 10;
+  private static final int DELAY = 2;
+  // IP address of the java servlet server
+  private static final String IPAddr = "demo-alb-1739993920.us-west-2.elb.amazonaws.com";
+  private static final String FILE_PATH = "/Users/nuanxin/Desktop/Example.jpg";
 
-  public static void main(String[] args) throws InterruptedException {
-    if (args.length != 4) {
-      System.out.println(
-          "Usage: java HW4aServer <threadGroupSize> <numThreadGroups> <delay> <IPAddr>");
-      System.exit(1);
-    }
-    int groupSize = Integer.parseInt(args[0]);
-    int numGroups = Integer.parseInt(args[1]);
-    int delay = Integer.parseInt(args[2]);
-    String IPAddr = args[3];
-
+  public static void main(String[] args) {
     PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
     connectionManager.setMaxTotal(1000);
     connectionManager.setDefaultMaxPerRoute(1000);
-    // Create an instance of HttpClient.
 
     CloseableHttpClient client = HttpClients.custom().setConnectionManager(connectionManager)
-        .setRetryStrategy(new DefaultHttpRequestRetryStrategy(5, TimeValue.ofSeconds(2))).build();
+            .setRetryStrategy(new DefaultHttpRequestRetryStrategy(5, TimeValue.ofSeconds(2))).build();
 
     try {
       SparkSession spark =
-          SparkSession.builder().appName("data").config("spark.master", "local").getOrCreate();
+              SparkSession.builder().appName("data").config("spark.master", "local").getOrCreate();
       spark.sparkContext().setLogLevel("ERROR");
       StructType schema =
-          new StructType().add("start", "long").add("requestType", "string").add("latency", "long")
-              .add("code", "integer");
+              new StructType().add("start", "long").add("requestType", "string").add("latency", "long")
+                      .add("code", "integer");
       List<Row> list = Collections.synchronizedList(new ArrayList<>());
-      File file = new File("/Users/boyuansun/Desktop/25Spring/CS 6650/Example.jpg");
+      File file = new File(FILE_PATH);
       ClientGet clientGet = new ClientGet(IPAddr, client, list);
       ClientPost clientPost = new ClientPost(IPAddr, client, list, file);
-      CountDownLatch completed = new CountDownLatch(10);
-      for (int i = 0; i < 10; i++) {
-        // lambda runnable creation - interface only has a single method so lambda works fine
-        Runnable thread = () -> {
-          for (int j = 0; j < 100; j++) {
-            clientGet.run();
-            clientPost.run();
-          }
-          completed.countDown();
-        };
-        new Thread(thread).start();
 
-      }
+//      CountDownLatch completed = new CountDownLatch(10);
+//      for (int i = 0; i < 10; i++) {
+//        Runnable thread = () -> {
+//          for (int j = 0; j < 100; j++) {
+//            clientGet.run();
+//            clientPost.run();
+//          }
+//          completed.countDown();
+//        };
+//        new Thread(thread).start();
+//      }
+//
+//      completed.await();
 
-      completed.await();
-
-      int totalThreads = groupSize * numGroups;
+      int totalThreads = GROUP_SIZE * NUMBER_OF_GROUPS;
 
       CountDownLatch completed2 = new CountDownLatch(totalThreads);
       long start = System.currentTimeMillis();
-      for (int j = 0; j < numGroups; j++) {
-        for (int i = 0; i < groupSize; i++) {
+      for (int j = 0; j < NUMBER_OF_GROUPS; j++) {
+        for (int i = 0; i < GROUP_SIZE; i++) {
           Runnable thread = () -> {
-            for (int k = 0; k < 1000; k++) {
+            for (int k = 0; k < 1; k++) {
               clientGet.run();
               clientPost.run();
             }
@@ -79,8 +74,8 @@ public class Main {
           new Thread(thread).start();
           //System.out.println("Thread started");
         }
-        if (j != numGroups - 1) {
-          Thread.sleep(delay * 1000);
+        if (j != NUMBER_OF_GROUPS - 1) {
+          Thread.sleep(DELAY * 1000);
         }
       }
       completed2.await();
@@ -95,9 +90,9 @@ public class Main {
       Dataset<Row> getMax = data.filter("requestType == 'GET'").groupBy().max("latency");
       Dataset<Row> postMax = data.filter("requestType == 'POST'").groupBy().max("latency");
       double[] get5099 = data.filter("requestType == 'GET'").stat()
-          .approxQuantile("latency", new double[] {0.5, 0.99}, 0);
+              .approxQuantile("latency", new double[] {0.5, 0.99}, 0);
       double[] post5099 = data.filter("requestType == 'POST'").stat()
-          .approxQuantile("latency", new double[] {0.5, 0.99}, 0);
+              .approxQuantile("latency", new double[] {0.5, 0.99}, 0);
       System.out.println("GET Mean Latency: " + getMean.first().getDouble(0));
       System.out.println("POST Mean Latency: " + postMean.first().getDouble(0));
       System.out.println("GET Min Latency: " + getMin.first().getLong(0));
@@ -112,8 +107,18 @@ public class Main {
       spark.stop();
       System.out.println("Time taken: " + (end - start) / 1000 + "s");
       System.out.println(
-          "Throughput: " + ((totalThreads * 2000)) / ((end - start) / 1000) +
-              " requests/s");
+              "Throughput: " + ((totalThreads * 2000)) / ((end - start) / 1000) +
+                      " requests/s");
+
+      int postSuccesses = ClientPost.getSuccessCount();
+      int postFails = ClientPost.getFailCount();
+      int getSuccesses = ClientGet.getSuccessCount();
+      int getFails = ClientGet.getFailCount();
+      System.out.println("Number of successful GET requests: " + getSuccesses);
+      System.out.println("Number of failed GET requests: " + getFails);
+      System.out.println("Number of successful requests: " + postSuccesses);
+      System.out.println("Number of failed requests: " + postFails);
+
     } catch (Exception e) {
       e.printStackTrace();
     }
